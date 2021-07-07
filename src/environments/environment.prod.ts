be4bfa70eba6ea12decabfdabae11d1de64288da
Aman <amanrawat882@gmail.com>
2021-07-07 09:13:13 +0530

    asd

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  apiBaseUrlLogin:'http://13.235.133.10:8084/api/auth/login',
-  production: true
+  production: true,
+  apiBaseUrlLogin:'http://13.235.133.10:8084/api/auth/login'
 };
